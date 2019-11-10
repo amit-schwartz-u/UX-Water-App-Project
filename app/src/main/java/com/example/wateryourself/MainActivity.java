cd15977ec760901291c620b510dbcf5920bed7d3
amit-schwartz-u <57283327+amit-schwartz-u@users.noreply.github.com>
2019-11-10 20:15:53 +0200

    save files

--- app/src/main/java/com/example/wateryourself/MainActivity.java
+++ app/src/main/java/com/example/wateryourself/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.wateryourself;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final int TEXT_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivityForResult(intent, TEXT_REQUEST);
     }
 }
