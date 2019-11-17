4882e1086d1def5c2b9c9098c768f08d26130ab1
amit-schwartz-u <57283327+amit-schwartz-u@users.noreply.github.com>
2019-11-17 19:25:01 +0200

    adding time out to main activity
    adding time out to main activity

--- app/src/main/java/com/example/wateryourself/LoginActivity.java
+++ app/src/main/java/com/example/wateryourself/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.wateryourself;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        //todo
     }
 }
--- app/src/main/java/com/example/wateryourself/MainActivity.java
+++ app/src/main/java/com/example/wateryourself/MainActivity.java
@@ -4,15 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     public static final int TEXT_REQUEST = 1;
+    private static int TIME_OUT = 3000; //Time to launch the another activity
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);//todo add if
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
     }
 
     public void launchSecondActivity(View view) {
