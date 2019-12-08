792475ffd2aadaa24c34d96cc39bb59a04365f5e
edenbrillant <39674183+edenbrillant@users.noreply.github.com>
2019-12-08 19:35:50 +0200

    new skip button

--- app/src/main/java/com/example/wateryourself/CupsActivity.java
+++ app/src/main/java/com/example/wateryourself/CupsActivity.java
@@ -32,8 +32,8 @@ public class CupsActivity extends AppCompatActivity {
 //    int[] counters = {0, 0, 0, 0};
     int[] amounts = {200, 500, 750, 1000};
 
-    Button mUpdtaeButton;
-    Button mCleanAllButton;
+    Button mUpdateButton;
+    Button mSkipButton;
 //    TextView[] tv_display_amount = new TextView[4];
 
     // Shared preferences object
@@ -52,10 +52,10 @@ public class CupsActivity extends AppCompatActivity {
         fillImagesId();
         Intent intent = getIntent();
         updateUserName(intent);
-        mUpdtaeButton =  findViewById(R.id.update_button);
-        mCleanAllButton = findViewById(R.id.clean_all_btn);
-        changeBtnMode(mUpdtaeButton,View.INVISIBLE);
-        changeBtnMode(mCleanAllButton,View.INVISIBLE);
+        mUpdateButton =  findViewById(R.id.update_button);
+        mSkipButton = findViewById(R.id.skip_to_my_plant_btn);
+        changeBtnMode(mUpdateButton,View.INVISIBLE);
+        changeBtnMode(mSkipButton,View.VISIBLE);
 
     }
     private void fillImageButtonsAmountOfWater() {
@@ -103,59 +103,11 @@ public class CupsActivity extends AppCompatActivity {
         btn.setVisibility(mode);
     }
 
-//    private void makeInvisibleIV() {
-//        littleImageViews[0] = (ImageView) findViewById(R.id.iv_littlecup200);
-//        littleImageViews[1] = (ImageView) findViewById(R.id.iv_littlecup500);
-//        littleImageViews[2] = (ImageView) findViewById(R.id.iv_littlebottle750);
-//        littleImageViews[3] = (ImageView) findViewById(R.id.iv_littlebottle1);
-//        for (int i = 0; i < littleImageViews.length; i++) {
-//            littleImageViews[i].setVisibility(View.INVISIBLE);
-//        }
-//    }
-//
-//    public void addAmount(View view) {
-//        changeBtnMode(View.VISIBLE);
-//
-//        //prevent user from choosing more than 3 of each cup/bottle
-//        if (counters[currentImage]==3){
-//            Toast toast = Toast.makeText(getApplicationContext(),
-//                    "That's too much! You can't pick more than 3",
-//                    Toast.LENGTH_LONG);
-//            toast.show();
-//        }
-//
-//        else{
-//            counters[currentImage]++;
-//        }
-//        if (counters[currentImage] == 1) {
-//            littleImageViews[currentImage].setVisibility(View.VISIBLE);
-//        }
-//        tv_display_amount[currentImage].setText(Integer.toString(counters[currentImage]));
-//        Button updateButton = findViewById(R.id.update_button);
-//        updateButton.setVisibility(View.VISIBLE);
-//        int totalAmountChosen = 0; //todo: reuse of this value also in launch daily
-//        for (int i = 0; i < counters.length; i++) {
-//            totalAmountChosen += counters[i] * amounts[i];
-//        }
-//        TextView totalAmountText = findViewById(R.id.tv_total_amount_chosen);
-//        totalAmountText.setText(String.format("Total Amount Chosen: %s ML", Integer.toString(totalAmountChosen)));
-//
-//    }
-
-    public void cleanAmountChosen(View view) {
-//        for (int i = 0; i < counters.length; i++) {
-//            counters[i] = 0;
-//            littleImageViews[i].setVisibility(View.INVISIBLE);
-//            tv_display_amount[i].setText("");
-//        }
-        changeBtnMode(mCleanAllButton,View.INVISIBLE);
-        changeBtnMode(mUpdtaeButton,View.INVISIBLE);
-        TextView totalAmountText = findViewById(R.id.tv_total_amount_chosen);
-        totalAmountText.setText(String.format("Amount Chosen: 0 ML"));
+    public void skipToPlant(View view) {
+        launchDailyActivity(view);
     }
 
     public void launchDailyActivity(View view) {
-
 
         Intent intent = new Intent(this, DailyActivity.class);
         intent.putExtra(AMOUNT_OF_WATER, amountChosen);
@@ -180,11 +132,8 @@ public class CupsActivity extends AppCompatActivity {
                 imagesAmountOfWater[i].setImageDrawable(getResources().getDrawable(unpickedAmmountImg[i]));
             }
         }
-        TextView totalAmountText = findViewById(R.id.tv_total_amount_chosen);
-        totalAmountText.setText(String.format("Amount Chosen: %s ML", Integer.toString(amountChosen)));
-        changeBtnMode(mCleanAllButton,View.VISIBLE);
-        changeBtnMode(mUpdtaeButton,View.VISIBLE);
-
+        changeBtnMode(mSkipButton,View.INVISIBLE);
+        changeBtnMode(mUpdateButton,View.VISIBLE);
 
     }
 }
