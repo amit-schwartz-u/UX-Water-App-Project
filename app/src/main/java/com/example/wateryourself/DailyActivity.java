b8b39d3351274dbab0d07f2737f9b34f73bcb8e7
ShaharBirzon <39333878+ShaharBirzon@users.noreply.github.com>
2019-12-08 21:58:01 +0200

    updating rain animation

--- app/src/main/java/com/example/wateryourself/DailyActivity.java
+++ app/src/main/java/com/example/wateryourself/DailyActivity.java
@@ -3,11 +3,13 @@ package com.example.wateryourself;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,8 +28,16 @@ public class DailyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily);
         Intent intent = getIntent();
+        int waterToAdd = intent.getIntExtra(CupsActivity.AMOUNT_OF_WATER, 0);
+        if (waterToAdd != 0){
+            ImageView rain = findViewById(R.id.im_rain);
+            ObjectAnimator animation = ObjectAnimator.ofFloat(rain, "translationY", 2000f);
+            animation.setDuration(3000);
+            animation.start();
+        }
+
         setCurrentWaterAmount(intent);
-        if (curWaterAmount >= 2500){
+        if (curWaterAmount >= 2500) {
             Toast toast = Toast.makeText(getApplicationContext(),
                     "Great job! you have reached your daily goal! ",
                     Toast.LENGTH_LONG);
@@ -36,47 +46,32 @@ public class DailyActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.tv_you_drank);
         textView.setText(String.format("You Drank: %s ML today !", curWaterAmount));
         setActivityBackgroundImage();
-        setWelcomeByNameInTop();
-    }
 
-    private void setWelcomeByNameInTop() {
-        TextView textView = findViewById(R.id.tv_hello_by_name);
-        textView.setText(String.format("Hi %s !", CupsActivity.mName));
     }
 
     private void setActivityBackgroundImage() {
         dailyConstraintLayout = findViewById(R.id.constraintLayout1); //todo make sure 250! not 200
-        if(curWaterAmount<250){ //todo try change to 1 line
+        if (curWaterAmount < 250) { //todo try change to 1 line
             dailyConstraintLayout.setBackgroundResource(R.drawable.daily_stage_0);
-        }
-        else if(curWaterAmount<500){
+        } else if (curWaterAmount < 500) {
             dailyConstraintLayout.setBackgroundResource(R.drawable.daily_stage_250);
-        }
-        else if(curWaterAmount<750){
+        } else if (curWaterAmount < 750) {
             dailyConstraintLayout.setBackgroundResource(R.drawable.daily_stage_500);
-        }
-        else if(curWaterAmount<1000){
+        } else if (curWaterAmount < 1000) {
             dailyConstraintLayout.setBackgroundResource(R.drawable.daily_stage_750);
-        }
-        else if(curWaterAmount<1250){
+        } else if (curWaterAmount < 1250) {
             dailyConstraintLayout.setBackgroundResource(R.drawable.daily_stage_1000);
-        }
-        else if(curWaterAmount<1500){
+        } else if (curWaterAmount < 1500) {
             dailyConstraintLayout.setBackgroundResource(R.drawable.daily_stage_1250);
-        }
-        else if(curWaterAmount<1750){
+        } else if (curWaterAmount < 1750) {
             dailyConstraintLayout.setBackgroundResource(R.drawable.daily_stage_1500);
-        }
-        else if(curWaterAmount<2000){
+        } else if (curWaterAmount < 2000) {
             dailyConstraintLayout.setBackgroundResource(R.drawable.daily_stage_1750);
-        }
-        else if(curWaterAmount<2250){
+        } else if (curWaterAmount < 2250) {
             dailyConstraintLayout.setBackgroundResource(R.drawable.daily_stage_2000);
-        }
-        else if(curWaterAmount<2500){
+        } else if (curWaterAmount < 2500) {
             dailyConstraintLayout.setBackgroundResource(R.drawable.daily_stage_2250);
-        }
-        else {
+        } else {
             dailyConstraintLayout.setBackgroundResource(R.drawable.daily_stage_2500);
         }
     }
